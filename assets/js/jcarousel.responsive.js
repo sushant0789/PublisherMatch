faa6850cdcd2db91bb2ed635ce2ddb8ac9153b65
sushant00789 <sushantkhairnar7@gmail.com>
2020-01-04 17:08:14 +0530

    first commit

---
+++ assets/js/jcarousel.responsive.js
@@ -0,0 +1,102 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 420) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=3'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=3'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 3,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+    });
+})(jQuery);
+
+
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel2');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 420) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev2')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next2')
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination2')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }
+            });
+    });
+})(jQuery);
+
+
---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+$('.btn-get-help').click(function() {
+        $('.left-side-banner').hide();
+        $('.left-side-banner22, .left-side-banner22 .first-step').show();
+    });
+
+    //$(window).resize(function(){
+    //    if ($(window).width() < 767){ 
+    //        $('.jcarousel-control-next').toggleClass('jcarousel-control-next2')
+    //    } 
+    //    if ($(window).width() > 767){ 
+    //        $('.jcarousel-control-next2').toggleClass('jcarousel-control-next')
+    //    } 
+    //   });
+
+})
